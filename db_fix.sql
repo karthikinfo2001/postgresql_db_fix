358d9355422b2db77412a9bccc3f11f88498b303
karthikinfo2001 <karthikinfo2001@gmail.com>
2024-05-23 11:00:17 +0530

    Executing all in one test today db

--- db_fix.sql
+++ db_fix.sql
@@ -1,13 +1,13 @@
---CREATE TABLE IF NOT EXISTS hello (
---    id SERIAL PRIMARY KEY,
---    name VARCHAR(50),
---    age INT
---);
+CREATE TABLE IF NOT EXISTS hello (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
 
---INSERT INTO hello (name, age) VALUES ('Alice', 30) ON CONFLICT DO NOTHING;
---INSERT INTO hello (name, age) VALUES ('Bob', 25) ON CONFLICT DO NOTHING;
---alter table hello add column email varchar;
+INSERT INTO hello (name, age) VALUES ('Alice', 30) ON CONFLICT DO NOTHING;
+INSERT INTO hello (name, age) VALUES ('Bob', 25) ON CONFLICT DO NOTHING;
+alter table hello add column email varchar;
 
 alter table hello add column phone int;
---alter table hello add column address varchar;
---alter table hello add column sub_4 int;
+alter table hello add column address varchar;
+alter table hello add column sub_4 int;
